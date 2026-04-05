5eccfbbcb9d15c7cf010425d710f9510da4f3400
Dil0518 <dilrangi.s.karunathilaka@gmail.com>
2026-04-05 17:26:28 +0530

    Added billing-service microservice

---
+++ billing-service/src/test/java/com/patient/management/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient.management.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
