fc5ae7d3d2bcf724b734bda60eac0eb0bf0656db
Dilrangi Sankalpana <dilrangi.s.karunathilaka@gmail.com>
2026-08-08 16:02:58 +0530

    Minor code changes

--- integration-tests/src/test/java/LoginApiIntegrationTest.java
+++ integration-tests/src/test/java/LoginApiIntegrationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class AuthIntegrationTest {
+public class LoginApiIntegrationTest {
 
     @BeforeAll
     static void setUp() {
